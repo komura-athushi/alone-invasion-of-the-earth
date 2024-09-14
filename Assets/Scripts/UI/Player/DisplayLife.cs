8b44777a79df7dc39fea24e936f1a59441395a36
Macaroni777 <shino-shino2022@outlook.jp>
2024-09-14 10:07:29 +0900

    Clean DisplayLife

--- Assets/Scripts/UI/Player/DisplayLife.cs
+++ Assets/Scripts/UI/Player/DisplayLife.cs
@@ -5,12 +5,18 @@ using UnityEngine.UI;
 
 public class DisplayLife : MonoBehaviour
 {
-    float heartPaddingRatio = 1.3f;
-    int playerlife = 5;
+    // ScriptableObjectにする予定
+
+    // UI用のScriptableObjectがほしい
+    // ハートの間隔
+    private float heartPaddingRatio = 1.3f;
+
+    // private float playerHP = DataController.GetGameParam().playerHP; 
+    private int playerHP = 5;
 
     void InitializeDrawLife()
     {
-        for (int i = 1; i <= playerlife; i++)
+        for (int i = 1; i <= playerHP; i++)
         {
             // ハートの生成
             var heart = Instantiate((GameObject)Resources.Load("UI_Life"));
@@ -28,31 +34,33 @@ public class DisplayLife : MonoBehaviour
 
     void DrawLife()
     {
+        // テスト用でキー入力を使用しています。
         if (Input.GetKeyDown("u"))
         {
             // テスト用回復
-            playerlife++;
+            playerHP++;
 
             // ハートの生成
             var heart = Instantiate((GameObject)Resources.Load("UI_Life"));
 
             // ハートに識別するための命名
-            heart.name = "heart" + playerlife;
+            heart.name = "heart" + playerHP;
 
             // Canvasを親にハートを設置
             heart.transform.SetParent(this.transform, false);
 
             // ハートをi個分ずらす
-            heart.transform.position = new Vector3(heart.transform.position.x + playerlife * heartPaddingRatio, heart.transform.position.y, heart.transform.position.z);
+            heart.transform.position = new Vector3(heart.transform.position.x + playerHP * heartPaddingRatio, heart.transform.position.y, heart.transform.position.z);
         }
 
+        // テスト用でキー入力を使用しています。
         if (Input.GetKeyDown("j"))
         {
             // テスト用ダメージ
-            playerlife--;
+            playerHP--;
 
             // ハートを減らす
-            Destroy(GameObject.Find("heart" + (playerlife + 1)));
+            Destroy(GameObject.Find("heart" + (playerHP + 1)));
         }
     }
 
@@ -64,8 +72,8 @@ public class DisplayLife : MonoBehaviour
     }
 
     // Update is called once per frame
-    // テスト用でFixedUpdateをUpdateにしています。Fixedだとレスポンスが悪くなるためです。
-    void Update()
+    // テストでは、FixedUpdateをUpdateにしていました。Fixedだとレスポンスが悪くなるためです。
+    void FixedUpdate()
     {
         // playerLifeを常にチェックしてハートの数を更新
         DrawLife();
